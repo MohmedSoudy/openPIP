4ac67ef421801e6d2762f84244c26acb19df4eca
Mohmed Soudy <MohmedSoudy2009@gmail.com>
2025-08-07 18:22:41 +0200

    update the start script to run the dockerfile properly

--- Docker OpenPIP package/start.sh
+++ Docker OpenPIP package/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-chown -R www-data:www-data app/logs &&\
+cp -r ./Docker\ OpenPIP\ package/*
+sudo chown -R www-data:www-data app/logs &&\
 sudo chmod -R 777 web/uploads 
 docker-compose build &&\ 
 docker-compose up
